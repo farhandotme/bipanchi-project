b369238fb9efa49c18f4a5b5140927b5d3b3a950
farhandotme <princeofficial355@gmail.com>
2024-10-30 11:30:21 +0530

    register and login varification done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ dotenv.config({ path: "./.env" });
 const conn = require("./db/connectionDb");
 const session = require("express-session");
 const flash = require("connect-flash");
+const userModel = require("./models/userModel");
+const bcrypt = require("bcrypt");
 
 conn();
 
@@ -13,12 +15,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(session({
-  secret: "secret",
-  cookie: { maxAge: 60000 },
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "secret",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(flash());
 
 app.get("/", (req, res) => {
@@ -32,9 +36,14 @@ app.get("/register", (req, res) => {
 });
 
 // POST REGISTER
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   let { firstname, lastname, email, password, password2 } = req.body;
-  
+  const user = await userModel.findOne({ email });
+  if (user) {
+    req.flash("error", "Email already exists");
+    res.redirect("/register");
+  }
+
   if (!firstname || !lastname || !email || !password || !password2) {
     req.flash("error", "All fields are required");
     res.redirect("/register");
@@ -42,15 +51,50 @@ app.post("/register", (req, res) => {
     req.flash("error", "Passwords do not match");
     res.redirect("/register");
   } else {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) throw err;
+        const newUser = await userModel.create({
+          firstname,
+          lastname,
+          email,
+          password: hash,
+        });
+      });
+    });
     req.flash("success", "Registration Successful");
-    res.redirect("/login"); // Success message will be accessible via req.flash in /login
+    res.redirect("/login");
   }
 });
 
-
+// GET LOGIN
 app.get("/login", (req, res) => {
   let success_msg = req.flash("success");
-  res.render("login", { success_msg });
+  let login_error = req.flash("login-error");
+  res.render("login", { success_msg , login_error });
+});
+
+// POST LOGIN
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    req.flash("login-error", "Email not found");
+    res.redirect("/login");
+  } else {
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) throw err;
+      if (isMatch) {
+        req.session.user = user;
+        req.flash("success", "Login Successful");
+        res.redirect("/home");
+      } else {
+        req.flash("login-error", "Incorrect Password");
+        res.redirect("/login");
+      }
+    });
+  }
 });
 
 const port = process.env.PORT || 4000;
--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  firstname: {
+  lastname: {
     type: String,
   },
   email: {
